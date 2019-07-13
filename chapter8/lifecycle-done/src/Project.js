d161a9f528df606bb91112552afd1ef336a03cdd
謝政宏 <b3194359@gmail.com>
2019-07-13 23:49:23 +0800

    lifecycle 範例

--- chapter8/lifecycle-done/src/Project.js
+++ chapter8/lifecycle-done/src/Project.js
@@ -4,17 +4,38 @@ class Product extends Component {
     render(){
         return(
           <div>
-            <p>Product</p>
+            <Cart />
           </div>
         );
     }
 }
 
 class Cart extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      qty: 0
+    }
+  }
+
+  updateQty = () => (
+    this.setState((state) => (
+      { qty: state.qty + 1 }
+    ))
+  )
+  componentDidMount(){
+    console.log('元件渲染之後執行');
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log('元件已更新')
+  }
+
   render(){
     return (
       <div>
-        <h1>Cart Items</h1>
+        <h1>Cart Items { this.state.qty }</h1>
+        <button onClick = { this.updateQty }>更新</button>
       </div>
     );
   }
