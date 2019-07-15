361ca74d65ed33dac028139cd164b83c7c94b195
謝政宏 <b3194359@gmail.com>
2019-07-15 14:48:06 +0800

    lifecycle 學習完畢

--- chapter8/lifecycle-done/src/Project.js
+++ chapter8/lifecycle-done/src/Project.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
+    constructor(props){
+      super(props);
+      this.state = {productId:'', qty: 0, isCart: true}
+    }
+    addToCart = (pid) => (
+      this.setState((state) => (
+        {
+          productId: pid,
+          qty: state.qty + 1
+        }
+      ))
+    )
+
+    removeCart = () => (
+      this.setState({isCart: false})
+    )
+
     render(){
         return(
           <div>
-            <Cart />
+            <button onClick={() => this.addToCart(1)}>Add to cart</button>
+            <button onClick={() => this.addToCart(2)}>Add to cart</button>
+            <button onClick={() => this.addToCart(3)}>Add to cart</button>
+            { this.state.isCart && <Cart productId={this.state.productId} qty={this.state.qty}/> }
+            { !this.state.isCart && <h2>Cart has been removed</h2>}
+            <button type="button" onClick={this.removeCart}>remove Cart</button>
           </div>
         );
     }
@@ -14,21 +36,44 @@ class Cart extends Component {
   constructor(props){
     super(props);
     this.state = {
-      qty: 0
+      qty: this.props.qty
     }
   }
 
-  updateQty = () => (
-    this.setState((state) => (
-      { qty: state.qty + 1 }
-    ))
-  )
+  static getDeriveStateFromProps(props, state){
+    if(props.qty !== state.qty){
+      return {qty: props.qty}
+    }
+
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.props.qty !== nextProps.qty){
+      console.log('元件已更新');
+      return true;
+    }
+    return false;
+  }
+
+  // updateQty = () => (
+  //   this.setState((state) => (
+  //     { qty: state.qty + 1 }
+  //   ))
+  // )
+
   componentDidMount(){
     console.log('元件渲染之後執行');
   }
 
   componentDidUpdate(prevProps, prevState){
-    console.log('元件已更新')
+    if(this.props.productId !== prevProps.productId){
+      console.log('元件已更新')
+    }
+  }
+
+  componentWillUnmount(){
+    console.log('Component 失效');
   }
 
   render(){
