aaa8200ecb91a6763357cf7f31fbc69386bcdbeb
謝政宏 <b3194359@gmail.com>
2019-07-17 23:59:09 +0800

    番茄鐘主要計時功能

--- chapter8/tomatotime/src/App.js
+++ chapter8/tomatotime/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Timer from './components/Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Timer />
     </div>
   );
 }
---
+++ chapter8/tomatotime/src/components/Timer.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class PomodoroTimer extends React.Component{
+
+    constructor(){
+      super();
+      this.state = {timeElapsed: 0}
+    }
+
+    totalTime(timeOne, timeTwo){
+        return timeOne + timeTwo;
+    }
+
+    componentDidMount() {
+        // console.log(new Date());
+        this.setInterval = setInterval(this.elapseTime.bind(this),1000)
+        this.setState({start: new Date()});
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+
+    elapseTime(){
+        //How much time has elapsed?
+        var currentTime = new Date();
+        // console.log("CURRENT" + currentTime);
+        // console.log(this.state.start);
+        //var timeElapsed = CURRENT TIME - START TIME
+        var timeElapsed = Math.floor((currentTime - this.state.start) / 1000);
+        console.log("timeElapsed" + timeElapsed);
+        this.setState({timeElapsed: timeElapsed});
+        console.log(this);
+
+        //if timeElapsed >= 25 minutes -> then alert
+        if(this.state.timeElapsed >= this.props.workingTime * 60){
+          clearInterval(this.interval);
+          alert("Time up for a break!");
+
+        }
+
+    }
+
+    render(){
+      return(
+        <div>This timer runs for {this.props.workingTime} minutes,
+        followed by rest of {this.props.restingTime} minutes.
+        <br />
+        For a total time of {this.totalTime(this.props.workingTime, this.props.restingTime)} minutes.
+        <br />
+        <br />
+        <div id="seconds">{this.state.timeElapsed}</div></div>
+      )
+    }
+
+}
+
+export default PomodoroTimer;
